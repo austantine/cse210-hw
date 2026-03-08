bdae9580c8db5a32fffe53d4ae9f6421462d0ce6
austantine <austantine@gmail.com>
2026-03-08 07:03:25 +0100

    Completed Exercise1 Project

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("::::::: Hello World! This is the Exercise1 Project.::::::");
+
+        Console.Write("What is your firsts name? ");
+        string fname = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+        Console.WriteLine(" ");
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
+        Console.WriteLine(" ");
+
+        Console.WriteLine("::::::: Successful Exercise1 Project.::::::");
     }
 }
