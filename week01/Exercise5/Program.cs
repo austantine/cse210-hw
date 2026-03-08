c0994176118c11bf258626f72b10cca40c5e0494
austantine <austantine@gmail.com>
2026-03-09 00:21:42 +0100

    successfully completed Exercise 5 project

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine(":::::: This is Functions and Methods for Exercise5 Project. :::::::");
+
+        // Call the helper methods in sequence
+        DisplayWelcome();
+
+        string name = PromptUserName();
+
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int squared = SquareNumber(number);
+
+        DisplayResult(name, squared);
+    }
+
+    // Method to display a welcome message
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    // Method to prompt the user for their name and return it
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    // Method to square a number and return the result
+    static int SquareNumber(int number)
+    {
+        int squared = number * number;
+        Console.WriteLine("The square of the number is: " + squared);
+        return squared;
+    }
+
+    // Method to display the final result
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine("Hello, " + name + "! the square of your number is " + square);
+        Console.WriteLine("");
+        Console.WriteLine(":::::::::: Thank you for using the program. Goodbye!::::::::::");
     }
 }
